ff85ce641db6c11c888cc010fbae1f8d158173d2
Viktor Tselov <viktor.tselov@gmail.com>
2024-09-24 01:36:40 +0300

    Add explicit Main() method to the App class

--- src/ResponsiveFlow.Application/App.xaml.cs
+++ src/ResponsiveFlow.Application/App.xaml.cs
@@ -1,9 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace ResponsiveFlow;
 
 public partial class App
 {
+    [STAThread]
+    internal static void Main()
+    {
+        var application = new App();
+        application.InitializeComponent();
+        application.Run();
+    }
+
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
