47f72e3b862b6198491fb1fb86c30770b7e09799
Viktor Tselov <viktor.tselov@gmail.com>
2024-09-23 03:31:32 +0300

    Add cancellation

--- src/ResponsiveFlow.Application/MainWindow.xaml.cs
+++ src/ResponsiveFlow.Application/MainWindow.xaml.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ResponsiveFlow;
 
 public partial class MainWindow
 {
+    private readonly MainWindowViewModel _viewModel;
+
     public MainWindow(MainWindowViewModel viewModel)
     {
         ArgumentNullException.ThrowIfNull(viewModel);
 
         InitializeComponent();
-        DataContext = viewModel;
+        DataContext = _viewModel = viewModel;
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        base.OnClosing(e);
+        _viewModel.CancelRun();
     }
 }
