bd5b6c12cb93f041f1a6a5a340dd4fd579a3ed68
Viktor Tselov <viktor.tselov@gmail.com>
2024-09-26 01:39:41 +0300

    Add MainModel

---
+++ src/ResponsiveFlow.Models/MainModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace ResponsiveFlow;
+
+public sealed class MainModel
+{
+    private readonly ILogger _logger;
+
+    public MainModel(ILogger<MainModel> logger)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+
+        _logger = logger;
+    }
+}
