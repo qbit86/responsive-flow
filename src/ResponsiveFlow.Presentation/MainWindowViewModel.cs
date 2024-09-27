2ebb36b0b8ada1fdcbf379f4045aff80b094961b
Viktor Tselov <viktor.tselov@gmail.com>
2024-09-27 21:29:15 +0300

    Improve version formatting

--- src/ResponsiveFlow.Presentation/MainWindowViewModel.cs
+++ src/ResponsiveFlow.Presentation/MainWindowViewModel.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 
 namespace ResponsiveFlow;
 
-public sealed class MainWindowViewModel
+public sealed partial class MainWindowViewModel
 {
     private readonly MainModel _model;
     private readonly AsyncRelayCommand _runCommand;
@@ -35,16 +32,5 @@ public sealed class MainWindowViewModel
     }
 
     private static string CreateTitle() =>
-        TryGetVersion(out string? version) ? $"{nameof(ResponsiveFlow)} {version}" : nameof(ResponsiveFlow);
-
-    private static bool TryGetVersion([NotNullWhen(true)] out string? version)
-    {
-        var assembly = typeof(MainWindowViewModel).Assembly;
-        var attributes = assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute));
-        var informationalVersions =
-            attributes.OfType<AssemblyInformationalVersionAttribute>().Select(it => it.InformationalVersion);
-        string? informationalVersion = informationalVersions.FirstOrDefault(it => !string.IsNullOrWhiteSpace(it));
-        version = informationalVersion ?? assembly.GetName().Version?.ToString();
-        return !string.IsNullOrWhiteSpace(version);
-    }
+        TryGetVersion(out string? version) ? $"{nameof(ResponsiveFlow)} v{version}" : nameof(ResponsiveFlow);
 }
