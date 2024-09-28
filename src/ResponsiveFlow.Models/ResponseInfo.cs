68777d96a882659c84931f48183bb2910f889ba3
Viktor Tselov <viktor.tselov@gmail.com>
2024-09-29 01:32:32 +0300

    Improve ResponseInfo.ToString()

--- src/ResponsiveFlow.Models/ResponseInfo.cs
+++ src/ResponsiveFlow.Models/ResponseInfo.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Diagnostics;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ResponsiveFlow;
@@ -10,4 +12,35 @@ public sealed record ResponseInfo(
     int AttemptIndex,
     long StartingTimestamp,
     Task<long> EndingTimestampFuture,
-    Task<HttpResponseMessage> ResponseFuture);
+    Task<HttpResponseMessage> ResponseFuture)
+{
+    public override string ToString()
+    {
+        StringBuilder builder = new();
+        builder.Append(nameof(ResponseInfo));
+        builder.Append(" { ");
+        if (PrintMembers(builder))
+            builder.Append(' ');
+        builder.Append('}');
+        return builder.ToString();
+    }
+
+    private bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append("UriIndex = ").Append(UriIndex);
+        builder.Append(", Uri = ").Append(Uri);
+        builder.Append(", AttemptIndex = ").Append(AttemptIndex);
+        if (EndingTimestampFuture.IsCompletedSuccessfully)
+        {
+            var elapsedTime = Stopwatch.GetElapsedTime(StartingTimestamp, EndingTimestampFuture.Result);
+            builder.Append(", ElapsedTime = ").Append(elapsedTime.TotalMilliseconds).Append("ms");
+        }
+        else
+        {
+            builder.Append(", StartingTimestamp = ").Append(StartingTimestamp);
+        }
+
+        builder.Append(", ResponseFuture.Status = ").Append(ResponseFuture.Status);
+        return true;
+    }
+}
