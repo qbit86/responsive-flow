dffb05f24569616a3d14854304d6a185eca19bb6
Viktor Tselov <viktor.tselov@gmail.com>
2024-09-28 14:13:43 +0300

    Add URIs and response channel

--- src/ResponsiveFlow.Models/ProjectRunner.cs
+++ src/ResponsiveFlow.Models/ProjectRunner.cs
@@ -1,34 +1,55 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using ResponseFuture = System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage>;
 
 namespace ResponsiveFlow;
 
 internal sealed partial class ProjectRunner
 {
+    private const int ResponseChannelCapacity = 32;
+
     private readonly ILogger _logger;
     private readonly ProjectDto _projectDto;
+    private readonly Channel<ResponseFuture> _responseChannel;
+    private readonly List<Uri> _uris;
 
-    private ProjectRunner(ProjectDto projectDto, string effectiveOutputDirectory, ILogger logger)
+    private ProjectRunner(ProjectDto projectDto,
+        List<Uri> uris,
+        string outputDirectory,
+        Channel<ResponseFuture> responseChannel,
+        ILogger logger)
     {
         _projectDto = projectDto;
-        OutputDirectory = effectiveOutputDirectory;
+        _uris = uris;
+        OutputDirectory = outputDirectory;
+        _responseChannel = responseChannel;
         _logger = logger;
     }
 
+    internal IReadOnlyList<Uri> Uris => _uris;
+
     internal string OutputDirectory { get; }
+
+    internal ChannelReader<ResponseFuture> ResponseChannelReader => _responseChannel.Reader;
+
+    internal ChannelWriter<ResponseFuture> ResponseChannelWriter => _responseChannel.Writer;
 
     internal static ProjectRunner Create(ProjectDto projectDto, ILoggerFactory loggerFactory)
     {
         ArgumentNullException.ThrowIfNull(projectDto);
         ArgumentNullException.ThrowIfNull(loggerFactory);
 
-        var logger = loggerFactory.CreateLogger<ProjectRunner>();
+        var validUris = GetValidUris(projectDto);
         var startTime = DateTime.Now;
         string effectiveOutputDirectory = GetOutputDirectoryOrFallback(projectDto, startTime);
-        return new(projectDto, effectiveOutputDirectory, logger);
+        var responseChannel = Channel.CreateBounded<ResponseFuture>(ResponseChannelCapacity);
+        var logger = loggerFactory.CreateLogger<ProjectRunner>();
+        return new(projectDto, validUris, effectiveOutputDirectory, responseChannel, logger);
     }
 
     internal async Task<Report> RunAsync(CancellationToken cancellationToken)
@@ -51,5 +72,18 @@ internal sealed partial class ProjectRunner
             return Path.Join(outputRoot, basename);
         return Path.Join(
             Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nameof(ResponsiveFlow), basename);
+    }
+
+    private static List<Uri> GetValidUris(ProjectDto projectDto)
+    {
+        string[] urlStrings = projectDto.GetUrlsOrEmpty();
+        List<Uri> uris = new(urlStrings.Length);
+        foreach (string urlString in urlStrings)
+        {
+            if (Uri.TryCreate(urlString, UriKind.Absolute, out var uri))
+                uris.Add(uri);
+        }
+
+        return uris;
     }
 }
