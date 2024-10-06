1f74bb730955333ae9479c6898b79832e63e0a85
Viktor Tselov <viktor.tselov@gmail.com>
2024-10-06 12:11:54 +0300

    Refactor BuildThenSaveHistogramAsync() using policies

--- src/ResponsiveFlow.Models/ProjectRunner.Histogram.cs
+++ src/ResponsiveFlow.Models/ProjectRunner.Histogram.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -20,25 +19,27 @@ internal sealed partial class ProjectRunner
         if (uriCollectedData.Sample is not { } sample)
             return;
 
+        var histogramPolicy = HistogramPolicy.Instance;
+        var histogramBinPolicy = HistogramBinPolicy.Instance;
         var histogram = SimpleHistogramBuilder.Instance.Build(sample.Values);
         (int uriIndex, var uri, _) = uriCollectedData;
-        IReadOnlyList<HistogramBin> bins = histogram.Bins;
+        var bins = histogramPolicy.GetBins(histogram);
         const double margin = 8.0;
         const double pivotX = margin;
         const double pivotY = margin;
 
         const double maxRectWidthPx = 512.0;
         const double minRectHeightPx = 24.0;
-        double minRectHeightMs = bins.Min(it => it.Upper - it.Lower);
-        double maxRectHeightMs = bins.Max(it => it.Upper - it.Lower);
+        double minRectHeightMs = bins.Min(histogramBinPolicy.Gap);
+        double maxRectHeightMs = bins.Max(histogramBinPolicy.Gap);
         double maxRectHeightPx = minRectHeightPx * maxRectHeightMs / minRectHeightMs;
-        double maxRectWidthUnits = bins.Max(it => it.Count);
+        double maxRectWidthUnits = bins.Max(histogramBinPolicy.Height);
 
         double widthPixelsByUnits = maxRectWidthPx / maxRectWidthUnits;
         double heightPixelsByMs = maxRectHeightPx / maxRectHeightMs;
 
         XNamespace ns = "http://www.w3.org/2000/svg";
-        double plotHeight = bins.Sum(it => heightPixelsByMs * (it.Upper - it.Lower));
+        double plotHeight = bins.Sum(it => heightPixelsByMs * histogramBinPolicy.Gap(it));
         XElement root = new(ns + "svg",
             new XAttribute("version", "2"),
             new XAttribute("width", maxRectWidthPx + 2 * margin),
@@ -49,8 +50,8 @@ internal sealed partial class ProjectRunner
         for (int i = 0; i < bins.Count; ++i)
         {
             var bin = bins[i];
-            double width = widthPixelsByUnits * bin.Count;
-            double height = heightPixelsByMs * (bin.Upper - bin.Lower);
+            double width = widthPixelsByUnits * histogramBinPolicy.Height(bin);
+            double height = heightPixelsByMs * histogramBinPolicy.Gap(bin);
 
             XElement rect = new(ns + "rect",
                 new XAttribute("width", width),
