31d3838bb78a5244595748d7f09c3d793f682224
Viktor Tselov <viktor.tselov@gmail.com>
2024-10-06 16:47:44 +0300

    Add captions

--- src/ResponsiveFlow.Models/Histograms/SvgHistogramBuilder.cs
+++ src/ResponsiveFlow.Models/Histograms/SvgHistogramBuilder.cs
@@ -6,6 +6,18 @@ using System.Xml.Linq;
 
 namespace ResponsiveFlow;
 
+file static class SvgHistogramBuilder
+{
+    internal const string Style =
+        """
+
+        text {
+          font-family: sans-serif;
+        }
+
+        """;
+}
+
 internal sealed class SvgHistogramBuilder<TBin>
 {
     private IReadOnlyList<string> _colors = SvgHistogramColors.Blue;
@@ -24,11 +36,12 @@ internal sealed class SvgHistogramBuilder<TBin>
         where TBinPolicy : IBinPolicy<TBin>
     {
         const double margin = 8.0;
-        const double pivotX = margin;
-        const double pivotY = margin;
-
         const double maxRectWidthPx = 512.0;
         const double minRectHeightPx = 24.0;
+        const double captionWidth = 120.0;
+        const double pivotX = margin + captionWidth;
+        const double pivotY = margin;
+
         double minRectHeightMs = bins.Min(it => binPolicy.Gap(it));
         double maxRectHeightMs = bins.Max(it => binPolicy.Gap(it));
         double maxRectHeightPx = minRectHeightPx * maxRectHeightMs / minRectHeightMs;
@@ -39,10 +52,11 @@ internal sealed class SvgHistogramBuilder<TBin>
 
         XNamespace ns = "http://www.w3.org/2000/svg";
         double plotHeight = bins.Sum(it => heightPixelsByMs * binPolicy.Gap(it));
-        XElement root = new(ns + "svg",
+        XElement svgElement = new(ns + "svg",
             new XAttribute("version", "2"),
-            new XAttribute("width", maxRectWidthPx + 2 * margin),
-            new XAttribute("height", plotHeight + 2 * margin));
+            new XAttribute("width", captionWidth + maxRectWidthPx + 3 * margin),
+            new XAttribute("height", plotHeight + 2 * margin),
+            new XElement(ns + "style", SvgHistogramBuilder.Style));
 
         double y = pivotY;
         for (int i = 0; i < bins.Count; ++i)
@@ -57,11 +71,20 @@ internal sealed class SvgHistogramBuilder<TBin>
                 new XAttribute("x", pivotX),
                 new XAttribute("y", y),
                 new XAttribute("fill", Colors[i & 1]));
-            root.Add(rect);
+            svgElement.Add(rect);
+
+            char closingBracket = i == bins.Count - 1 ? ']' : ')';
+            FormattableString formattable = $"[{binPolicy.Lower(bin):F2}, {binPolicy.Upper(bin):F2}{closingBracket}";
+            XElement text = new(ns + "text",
+                new XAttribute("text-anchor", "end"),
+                new XAttribute("x", pivotX - margin),
+                new XAttribute("y", y + 16.0),
+                FormattableString.Invariant(formattable));
+            svgElement.Add(text);
 
             y += heightPx;
         }
 
-        return root;
+        return svgElement;
     }
 }
